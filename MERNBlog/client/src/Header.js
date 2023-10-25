b1012822ea6ad00308f4de2624a439b66731055f
KrzysztofProszek <37065502+KrzysztofProszek@users.noreply.github.com>
2023-10-26 00:33:31 +0200

    Added: API, Routing, Register/Login/Logout

--- MERNBlog/client/src/Header.js
+++ MERNBlog/client/src/Header.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+  const [username, setUsername] = useState(null)
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include'
+    }).then(response=>{
+      response.json().then(userInfo=>{
+setUsername(userInfo.username)
+      })
+    })
+  }, [])
+
+function logout(){
+  fetch('http://localhost:4000/logout', {
+    credentials:'include',
+    method:'POST'
+  })
+}
+
   return (
     <header>
       <Link to="/" className="logo">
@@ -8,8 +27,18 @@ export default function Header() {
       </Link>
 
       <nav>
-        <Link to="/login">Login</Link>
+{username && (
+  <>
+  <Link to='/create'>Create new post</Link>
+  <a onClick={logout}>Logout</a>
+  </>
+)}
+{!username &&(
+  <>
+  <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
+  </>
+)}
       </nav>
     </header>
   )
